f8189a694edadb9eadefec5d377798aef24479d9
Mathias Bär <mathiasbaer@gmail.com>
2013-08-29 17:57:23 +0200

    performer/head_changer ansteuerbar

---
+++ performer/head_changer/head_changer_04/js/Parts.js
@@ -0,0 +1,78 @@
+function Head ( _el ) {
+  
+  this.isRunning = false;
+  this.el = $(_el);
+  this.posUp = { x: 0, y: -this.el.parent().height()};
+  //this.el.css({"top":this.pos.y + "px"});
+  
+  this.run = function( _d ) {
+    
+    var that = this;
+    this.isRunning = true;
+    
+    var t = {
+      up: Math.floor(_d*2/5),
+      wait: Math.floor(_d*1/5),
+      down: Math.floor(_d*2/5)
+    }
+    
+    this.el
+      // up
+      .animate(
+        {top: this.posUp.y},
+        t.up, 
+        "easeOutQuart",
+        // ==================================================== show random graphic
+        function() { that.el.find(">*").hide().random().show(); }
+      )
+      // wait
+      .delay( t.wait )
+      // down
+      .animate(
+        {top: 0 }, 
+        t.down, 
+        "easeOutQuart",
+        function() { that.isRunning = false; }
+      );
+  };
+  
+};
+
+
+function Arms ( _el ) {
+  
+  this.isRunning = false;
+  this.el = $(_el);
+  this.posUp = { x: 0, y: -this.el.parent().height()};
+  //this.el.css({"top":this.pos.y + "px"});
+  
+  this.run = function( _d ) {
+    
+    var that = this;
+    this.isRunning = true;
+    
+    var t = {
+      up: Math.floor(_d*2/5),
+      wait: Math.floor(_d*1/5),
+      down: Math.floor(_d*2/5)
+    }
+    
+    this.el
+      // up
+      .animate(
+        {top: this.posUp.y},
+        t.up, 
+        "easeOutQuart"
+      )
+      // wait
+      .delay( t.wait )
+      // down
+      .animate(
+        {top: 0 }, 
+        t.down, 
+        "easeOutQuart",
+        function() { that.isRunning = false; }
+      );
+  };
+  
+};
